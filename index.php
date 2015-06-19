3dc7c1834b03fb137d777ebb71cd3045244ab9ce
ewakot <kotanskae@gmail.com>
2015-06-19 14:30:49 +0200

    ostylowanie panelu logowania
    style

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ session_start();
 
 include("nagl.php");
 
+
 if(isset($_SESSION['login'])){
 	session_destroy();
 	$_SESSION = array();
@@ -10,15 +11,57 @@ if(isset($_SESSION['login'])){
 	}
 	?>
 	
-		<form action = "log2.php" method = "POST">
-			e-mail: <input type = "text" name = "email" /><br>
-			hasło: <input type = "password" name ="haslo" /><br>
-			<input type = "submit" value = "Zaloguj" />
+		<html >
+  <head>
+    <meta charset="UTF-8">
+    <title>Calm breeze login screen</title>
+    
+    
+		<link rel="stylesheet" href="css/style.css">
+    
+    
+    
+  </head>
+
+  <body>
+
+    <div class="wrapper">
+	<div class="container">
+		<h1>Welcome</h1>
+		
+		<form class="form" action = "log2.php" method = "POST" >
+			<input type="text" name="email">
+			<input type="password" name="haslo">
+			<input type="submit" id="login-button" value ="Zaloguj">
 		</form>
 		
 		<form action="register.php">
 			<input type="submit" value="Zarejestruj się" />
 		</form>
+	</div>
+	
+	<ul class="bg-bubbles">
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+	</ul>
+</div>
+    <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+
+    
+
+    
+    
+    
+  </body>
+</html>
 
 		<?
 
