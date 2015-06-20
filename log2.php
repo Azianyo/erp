dcc039adf62feafc6761a85bab16087ae1e0ac74
ewakot <kotanskae@gmail.com>
2015-06-20 12:02:26 +0200

    logowanie-szablon

--- log2.php
+++ log2.php
@@ -2,18 +2,14 @@
 	session_start();
 	include("nagl.php");
 	include ("polacz.php");
+
+	echo "<div class=\"wrapper\">";
+	echo "<div class=\"container\">";
 	
-	echo $_POST['email'] . "<br>";
-	echo $_POST['haslo'] . "<br>";
-	echo isset($_POST['email']);
-	print_r($_POST);
+
 
 	if(isset($_SESSION)){
-		echo "Zmienne sesji:<br>";
-		print_r($_SESSION);
-		echo "<br><br>";
-		print_r($_POST);
-		echo "<br><br>";
+		
 	}else
 		echo "Sesja nie zostala jeszcze zainicjowana <br><br>";
 		
@@ -73,7 +69,7 @@
 						$forma2.="</form>";
 
 						$forma3 ="<form action = \"calendar.php\">";
-						$forma3.= "<input type=\"submit\" value=\"Przeglądaj kalendarz\" />";
+						$forma3.= "<input type=\"submit\" id=\"login-button\" value=\"Przeglądaj kalendarz\" />";
 						$forma3.="</form>";
 
 						echo $forma;
@@ -95,11 +91,30 @@
 
 				echo $forma;
 				echo $button;
-			
-			}
+				
+						$buttonek="<form action=\"index.php\" method=\"POST\">";
+						$buttonek.= "<input type = \"submit\" value=\"Powrót \"/>";
+						$buttonek.= "</form>";
+				echo $buttonek;
+	
+}
 		if((isset($_POST['email']))&&(!((isset($has)) && md5($_POST['haslo']) == $has))){
 			echo "Podales zle dane - nie masz uprawnien";
 		}
+		echo "</div> <ul class=\"bg-bubbles\">
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+	</ul>
+	</div>
+    <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>"
 	?>
 	
 	<?
