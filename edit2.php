298d10a6aefdd8fbce98c1e0c7c8d52d007dcd33
Azianyo <sfrustrowanaherbatamietowa@gmail.com>
2015-06-23 16:25:08 +0200

    Generowanie wypisu pdf

--- edit2.php
+++ edit2.php
@@ -3,6 +3,11 @@
 	include("nagl.php");
 	include("polacz.php");
 	include("sessioncheck.php");
+	?>
+	<style type="text/css" media="print">
+   		.no-print { display: none; }
+	</style>
+	<?
 
 	echo "<h1>" . $_POST['imie'] . " " . $_POST['nazwisko'] . "</h1>";
 	if((isset($_SESSION['login']))&&(md5($_SESSION['login'])==$wiersz['haslo'])&&($_SESSION['nazwisko']==$wiersz['nazwisko'])&&($wiersz['uprawnienia'] == "1" || $wiersz['uprawnienia'] == "-1")){
@@ -27,28 +32,32 @@
 			}
 		}
 
-		$form1= "<form action=\"diagnose.php\" method=\"POST\">";
+		$form1= "<form action=\"diagnose.php\" class=\"no-print\" method=\"POST\">";
 		$form1.= "<input type=\"hidden\" name=\"id_pacjenta\" value=\"" . $_POST['id'] ."\" size=\"20\" maxlength=\"30\" />";
 		$form1.= "<input type=\"submit\" value=\"Postaw diagnozę\" />";
 		$form1.= "</form>";
 
-		$form2= "<form action=\"edit.php\">";
+		$form2= "<form action=\"edit.php\" class=\"no-print\">";
 		$form2.= "<input type=\"submit\" value=\"Wróć\" />";
 		$form2.= "</form>";	
 
-		$form3= "<form action=\"log2.php\">";
+		$form3= "<form action=\"log2.php\" class=\"no-print\">";
 		$form3.= "<input type=\"submit\" value=\"Wróć\" />";
 		$form3.= "</form>";			
 
 
 	?>
 
-	<a href="http://FreeHTMLtoPDF.com/?convert=http%3A%2F%2Fwww.student.agh.edu.pl/~borzemsk/erp/edit2.php">Download as PDF</a>
 
-	<form action="disease.php" method="POST">
+	<form action="disease.php" class="no-print" method="POST">
 	<input type="hidden" name="id_pacjenta" value=<?echo $_POST['id'];?> size="20" maxlength="30" />
 	<input type="submit" value="Dodaj przebytą chorobę" />
 	</form>
+
+	<form>
+		<input type="button" onClick="window.print()" class="no-print" value="Wydrukuj lub ściągnij wypis w PDF">
+	</form>
+
 
 	<?
 		if($wiersz['uprawnienia'] == "1"){
@@ -60,7 +69,7 @@
 		}
 	?>
 
-	<form action="wyloguj.php" method="POST">
+	<form action="wyloguj.php" class="no-print" method="POST">
 	<input type = "submit" value="Wyloguj"/>
 	</form>
 
